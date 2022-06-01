2cee94b7719c2d2656c887ca63d6192ad693a2d6
Naveen224466 <106344617+Naveen224466@users.noreply.github.com>
2022-06-01 20:29:28 +0530

    To find the max no between 3 numbers

---
+++ max_bw_3.py
@@ -0,0 +1,9 @@
+x=int(input("enter the no"))
+y=int(input("enter the no"))
+z=int(input("enter the no"))
+if(x>y and x>z):
+    print('x ia maximum')
+elif(y>x and y>z):
+    print('y is maximum')
+else:
+    print('z is maximum')
