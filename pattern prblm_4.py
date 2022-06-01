6faa46219b305ae9b4ccddc6060baa36de72c92a
Naveen224466 <106344617+Naveen224466@users.noreply.github.com>
2022-06-01 20:15:41 +0530

    write a python program to print the given pattern

---
+++ pattern prblm_4.py
@@ -0,0 +1,11 @@
+rows =5
+for i in range(1,rows+1):
+    for j in range(rows+1-i):
+        print('*',end='')
+    for j in range(i-1):
+        print(' ',end='')
+    for j in range(i-1):
+        print(' ',end='')
+    for j in range(rows+1-i):
+        print('*',end='')
+    print()
